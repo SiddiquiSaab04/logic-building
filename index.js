b9bb2148442e3afb3be4b9843be56bdb1f12df5f
SiddiquiSaab04 <devlaiba04@gmail.com>
2026-01-17 17:30:23 +0500

    flatten array, revers in 3 iteration , palindrome

---
+++ index.js
@@ -0,0 +1,48 @@
+// let arr = ['2','1','1','2','3'];
+
+// let reversedArr = [];
+
+// function checkPanlidrome(){
+//     for(let i=arr.length-1; i>=0; i--){
+//         reversedArr.push(arr[i]);
+//     }
+//     if(arr.join('') === reversedArr.join('')){
+//         console.log("The given word is a palindrome");
+//     }
+//     else{
+//         console.log("The given word is not a palindrome");
+//     }
+// }
+// checkPanlidrome();
+
+// we've an array  we've to reverse it only in 3 iterations we've an array of 6 elements
+
+// let array = [1,2,3,4,5,6,4];
+// let reversedArray=[];
+// function reverseIn3Iterations(){
+//     for(let i = array.length - 1; i >=0; i-=2){
+//        reversedArray.push(array[i], array[i-1]);
+//     }
+//     console.log(reversedArray);
+// }
+
+// reverseIn3Iterations();
+
+// we've to flatten an array
+
+let  array = [1,3,5,2,1,4,2,3,10,44];
+
+function flattenArray(){
+    for(let i=0; i<array.length; i++){
+        for(let j=i+1; j<array.length; j++){
+            if(array[i] === array[j]){
+                array.splice(j,1);
+            }
+            else{
+                continue;
+            }
+    }
+}
+console.log(array);
+}
+flattenArray();
